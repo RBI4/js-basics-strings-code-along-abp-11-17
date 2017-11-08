6d98a3f23fbfbf9056eb11bcc9bada759d63a182
RBI4 <rebecca.inge@gmail.com>
2017-11-08 17:02:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser}`;
+
+
